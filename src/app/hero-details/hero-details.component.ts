27e2ddc3e837fd1288bf14b22bec4f136f8f5dec
kunwar <s.kunwar@espresto.com>
2021-03-17 18:41:45 +0100

    services added

---
+++ src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,16 @@
+import { Hero } from '../hero';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-details',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.css']
+})
+export class HeroDetailsComponent implements OnInit {
+  @Input() hero!: Hero;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import {HeroService } from '../hero.service' ;
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
-  constructor() { }
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  heroes: Hero[] = [];
+  selectedHero!: Hero;
+  constructor(private heroService: HeroService) { }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
